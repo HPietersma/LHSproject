1a514fb360eafe156eb79cc156d67b0853113634
HPietersma <hessel.pietersma@hotmail.nl>
2022-02-17 09:07:14 +0100

    asfd

--- script2.js
+++ script2.js
@@ -1,10 +1,12 @@
 function openReviews(id) {
    
-    if (id.style.height == "300px") {
-        id.style.height = "30px";
+    if (id.parentElement.style.height == "300px") {
+        id.parentElement.style.height = "31px";
+        id.parentElement.children[1].style.visibility = "hidden";
     }
     else {
-        id.style.height = "300px";
+        id.parentElement.style.height = "300px";
+        id.parentElement.children[1].style.visibility = "visible";
     }
     console.log(id);
     //id.lastChild.style.height = "300px";
